3ce56aa0540a06c3f541a475c5b4222c529c6fa5
AltinBrryli <altinxhaka1@gmail.com>
2022-11-07 13:53:29 +0100

    extend Varehouse.java with RealmObject

--- app/src/main/java/org/planetaccounting/saleAgent/model/Varehouse.java
+++ app/src/main/java/org/planetaccounting/saleAgent/model/Varehouse.java
@@ -3,11 +3,13 @@ package org.planetaccounting.saleAgent.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import io.realm.RealmObject;
+
 /**
  * Created by macb on 06/02/18.
  */
 
-public class Varehouse {
+public class Varehouse extends RealmObject {
 
     @SerializedName("id")
     @Expose
