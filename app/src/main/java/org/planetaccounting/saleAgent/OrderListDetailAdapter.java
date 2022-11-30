9321ad87da0fef5347ed321cf3d4d2520947fcda
AltinBrryli <altinxhaka1@gmail.com>
2022-11-30 15:21:07 +0100

    fixed: cutTo2 per sasin , cmimin dhe vleren e porosise .

--- app/src/main/java/org/planetaccounting/saleAgent/OrderListDetailAdapter.java
+++ app/src/main/java/org/planetaccounting/saleAgent/OrderListDetailAdapter.java
@@ -1,5 +1,6 @@
 package org.planetaccounting.saleAgent;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 
 import org.planetaccounting.saleAgent.databinding.OrderlistDetailListItemBinding;
 import org.planetaccounting.saleAgent.model.OrderDetailItem;
+import org.planetaccounting.saleAgent.model.order.Order;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -20,6 +22,7 @@ public class OrderListDetailAdapter extends RecyclerView.Adapter<OrderListDetail
 
     ArrayList<OrderDetailItem> orderDetailItems = new ArrayList<>();
     private Context ctx;
+    private CancelOrder listener;
 
     public OrderListDetailAdapter(ArrayList<OrderDetailItem> orderDetailItems) {
         this.orderDetailItems = orderDetailItems;
@@ -33,20 +36,24 @@ public class OrderListDetailAdapter extends RecyclerView.Adapter<OrderListDetail
         return new OrderListDetailAdapter.ViewHolder(binding);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(OrderListDetailAdapter.ViewHolder holder, int position) {
         OrderlistDetailListItemBinding binding = holder.binding;
         double vVlera = Double.parseDouble(String.valueOf(orderDetailItems.get(position).getAmount()));
-        double vSasia = Double.parseDouble(String.valueOf(orderDetailItems.get(position).getQuantity()));
+        double vSasia = Float.parseFloat(String.valueOf(orderDetailItems.get(position).getQuantity()));
         double vCmimi = Double.parseDouble(String.valueOf(orderDetailItems.get(position).getPrice()));
 
         binding.shifra.setText(orderDetailItems.get(position).getNumber());
         binding.name.setText(orderDetailItems.get(position).getName());
+//        binding.sasia.setText(orderDetailItems.get(position).getQuantity());
         binding.sasia.setText(""+cutTo2(vSasia));
         binding.barkod.setText(orderDetailItems.get(position).getBarcode());
         binding.njesia.setText(orderDetailItems.get(position).getUnit());
         binding.cmimi.setText(""+cutTo2(vCmimi));
+//        binding.cmimi.setText(orderDetailItems.get(position).getPrice() + "");
         binding.vlera.setText(""+cutTo2(vVlera));
+//        binding.vlera.setText(orderDetailItems.get(position).getAmount() + "");
     }
 
     @Override
@@ -69,6 +76,10 @@ public class OrderListDetailAdapter extends RecyclerView.Adapter<OrderListDetail
             this.binding = binding;
 
         }
+    }
+
+    interface CancelOrder{
+        void onCancelPressed(Order order);
     }
 
     public double cutTo2(double value) {
